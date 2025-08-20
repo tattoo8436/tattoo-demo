74b48924d48534a8d09af7fd88e26f68507bde5c
Oscar <oscar.brightsoft@gmail.com>
2025-08-20 16:28:05 +0700

    change size

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
       const res = await fetch("/data/AirportData.json");
       const data: IAirport[] = await res.json();
 
-      const size = 50;
+      const size = 100;
       const loopCount = Math.ceil(data.length / size);
       const translatedData: IAirport[] = [];
 
